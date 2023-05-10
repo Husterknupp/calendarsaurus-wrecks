b94f002df915b9218feac4742fce5036b4089f21
Benjamin <bens@spreadshirt.net>
2023-05-10 13:17:45 +0200

    create example message

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mittwoch</h1>
+        <div>10. Mai 2023</div>
+        <div className={"today-info"}>Heute kein Mittagessen - wir holen dich 11 Uhr bei dir Zuhause ab.</div>
       </header>
     </div>
   );
