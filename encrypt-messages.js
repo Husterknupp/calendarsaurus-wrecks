f2018a7d3d81345819f64886dd33d3e4b08dd284
Benjamin <bens@spreadshirt.net>
2023-05-15 21:04:31 +0200

    automatically publish latest messages content

--- encrypt-messages.js
+++ encrypt-messages.js
@@ -1,12 +1,13 @@
 const CryptoJS = require("crypto-js");
 const fs = require("fs");
 
+let outDir = "public/messages-encrypted.txt";
+console.log("Encrypting messages. Find output here: ", outDir);
+
 const messages = fs.readFileSync("messages.json", { encoding: "utf-8" });
 // todo password to be entered instead
 const ciphertext = CryptoJS.AES.encrypt(messages, "secret key 123").toString();
 
-console.log(`ciphertext`, ciphertext);
-
-fs.writeFileSync("./public/messages-encrypted.txt", ciphertext, {
+fs.writeFileSync(outDir, ciphertext, {
   encoding: "utf-8",
 });
