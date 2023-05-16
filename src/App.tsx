0825488f5d4aacbb1d3d409a693fa99c216706c6
Benjamin <bens@spreadshirt.net>
2023-05-17 00:03:14 +0200

    rewrite encrypting, decrypting and polling of messages

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React, { ReactElement, useEffect, useState } from "react";
 import "./App.css";
 import dayjs from "dayjs";
 import LOCALE_DE from "dayjs/locale/de";
+import axios from "axios";
+import CryptoJS from "crypto-js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <DateCpt />
-        <div className={"today-info"}>
-          Heute kein Mittagessen - wir holen dich 11:30 Uhr bei dir Zuhause ab.
-        </div>
+        <MessageCpt />
       </header>
     </div>
   );
@@ -39,6 +39,41 @@ function DateCpt(): ReactElement {
       </div>
     </>
   );
+}
+
+type Message = {
+  date: string; // 2023-05-11
+  message: string;
+};
+
+async function getMessages(): Promise<Message[]> {
+  const encrypted = await axios.get("messages-encrypted.txt");
+  // todo password to be entered instead
+  const bytes = CryptoJS.AES.decrypt(encrypted.data, "secret key 123");
+  return JSON.parse(bytes.toString(CryptoJS.enc.Utf8)).data; // JSON standard allows the toplevel to be only objects
+}
+
+const TIME_1MIN = 60000;
+
+function MessageCpt(): ReactElement | null {
+  const [messages, setMessages] = useState<Message[]>([]);
+  useEffect(() => {
+    async function doRequest() {
+      // js: `<script defer="defer" src="/calendarsaurus-wrecks/static/js/main.39124231.js">`
+      // %PUBLIC_URL% -> /calendarsaurus-wrecks
+      setMessages(await getMessages());
+    }
+
+    doRequest();
+
+    const id = setInterval(doRequest, TIME_1MIN);
+    return () => clearInterval(id);
+  }, []);
+
+  const today = dayjs().format("YYYY-MM-DD");
+  const message = messages.find((m) => m.date === today);
+
+  return message ? <div className={"today-info"}>{message.message}</div> : null;
 }
 
 export default App;
