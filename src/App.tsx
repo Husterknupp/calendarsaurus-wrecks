a713de3140f9a0fb2e5468c840f7c00ef4018844
Benjamin <bens@spreadshirt.net>
2023-05-16 23:03:59 +0200

    rewrite Date Cpt pt I

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className={"weekday"}>Mittwoch</h1>
-        <div className={"date"}>10. Mai 2023</div>
-        <div className={"today-info"}>Heute kein Mittagessen - wir holen dich 11:30 Uhr bei dir Zuhause ab.</div>
+        <DateCpt />
+        <div className={"today-info"}>
+          Heute kein Mittagessen - wir holen dich 11:30 Uhr bei dir Zuhause ab.
+        </div>
       </header>
     </div>
+  );
+}
+
+const TIME_1SEC = 1000;
+
+function DateCpt(): ReactElement {
+  const [date, setDate] = useState(new Date().toString());
+
+  useEffect(() => {
+    function refreshDate() {
+      setDate(new Date().toString());
+    }
+    const id = setInterval(refreshDate, TIME_1SEC);
+    return () => clearInterval(id);
+  }, []);
+
+  return (
+    <>
+      <h1 className={"weekday"}>Montag</h1>
+      <div className={"date"}>{date}</div>
+    </>
   );
 }
 
