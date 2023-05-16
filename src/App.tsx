1f2cba16eb3c39c227097b2931cf0d046872a7d8
Benjamin <bens@spreadshirt.net>
2023-05-17 00:28:47 +0200

    Revert "start with polling for the messages resource"
    This reverts commit 0cffa483c6c8dac979299ca8c1c07952c44d544d.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React, { ReactElement, useEffect, useState } from "react";
 import "./App.css";
 import dayjs from "dayjs";
 import LOCALE_DE from "dayjs/locale/de";
-import axios from "axios";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <DateCpt />
-        <MessageCpt />
+        <div className={"today-info"}>
+          Heute kein Mittagessen - wir holen dich 11:30 Uhr bei dir Zuhause ab.
+        </div>
       </header>
     </div>
   );
@@ -39,49 +40,5 @@ function DateCpt(): ReactElement {
     </>
   );
 }
-
-type Message = {
-  date: string; // 2023-05-11
-  message: string;
-};
-
-const TIME_1MIN = 60000;
-
-function MessageCpt(): ReactElement | null {
-  const [messages, setMessages] = useState<string>();
-  useEffect(() => {
-    async function doRequest() {
-      setMessages(await axios.get("messages-encrypted.txt"));
-    }
-
-    doRequest();
-
-    const id = setInterval(doRequest, TIME_1MIN);
-    return () => clearInterval(id);
-  }, []);
-
-  return <div className={"today-info"}>{messages}</div>;
-}
-
-// function MessageCpt(): ReactElement | null {
-//   const [messages, setMessages] = useState<Message[]>([]);
-//   useEffect(() => {
-//     async function doRequest() {
-//       // js: `<script defer="defer" src="/calendarsaurus-wrecks/static/js/main.39124231.js">`
-//       // %PUBLIC_URL% -> /calendarsaurus-wrecks
-//       setMessages(await axios.get("messages-encrypted.txt"));
-//     }
-//
-//     doRequest();
-//
-//     const id = setInterval(doRequest, TIME_1MIN);
-//     return () => clearInterval(id);
-//   }, []);
-//
-//   const today = dayjs().format("YYYY-MM-DD");
-//   const message = messages.find((m) => m.date === today);
-//
-//   return message ? <div className={"today-info"}>{message.message}</div> : null;
-// }
 
 export default App;
